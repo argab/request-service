f09ee7dca7dcf873cffa5093d4ed950e08c537ac
argab <argabid@gmail.com>
2021-07-17 02:59:59 +0600

    1.1.1

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-module.exports = require('./dist');
+const {Request, AbstractRequest, RequestRepository, ClientDecorator, RequestDecorator, RequestMediator, RequestMediatorDecorator, RequestHandler, RequestFactory} = require('./dist');
+
+module.exports = {Request, AbstractRequest, RequestRepository, ClientDecorator, RequestDecorator, RequestMediator, RequestMediatorDecorator, RequestHandler, RequestFactory}
